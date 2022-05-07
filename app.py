b868d1555343a97db650257f6a0438bb31712bf0
GamePlayer103 <72652455+GamePlayer103@users.noreply.github.com>
2022-05-07 23:03:56 +0200

    adding new password to database

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 import sys
+import sqlite3
+
+DB_PATH = './passwords.db'
 
 def print_help():
     msg = (
@@ -9,7 +12,59 @@ def print_help():
     )
     print(msg)
 
+def create_connection(db_file):
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+        return conn
+    except Error as e:
+        print(e)
+
+    return conn
+
+def init_tables():
+    sql = """ CREATE TABLE IF NOT EXISTS passwords (
+                id integer PRIMARY KEY, 
+                name text,
+                email text,
+                username text,
+                password text,
+                notes text
+            ); """
+    
+    conn = create_connection(DB_PATH)
+    if conn is not None:
+        try:
+            c = conn.cursor()
+            c.execute(sql)
+        except Error as e:
+            print(e)
+    else:
+        print('error! cannot create database connection')
+
+def add_password():
+    try: 
+        name = input('name: ')
+        email = input('email: ')
+        username = input('username: ')
+        password = input('password: ')
+        notes = input('notes: ')
+    except KeyboardInterrupt:
+        print('\npressed ctrl-c, aborting')
+
+    data = (name, email, username, password, notes)
+    sql = ''' INSERT INTO passwords(name, email, username, password, notes)
+            VALUES(?,?,?,?,?)'''
+        
+    conn = create_connection(DB_PATH)
+    c = conn.cursor()
+    c.execute(sql, data)
+    conn.commit()
+
+
 if __name__ == '__main__':
+    init_tables()
+
     try:
         arg = sys.argv[1].lower()
     except IndexError:
@@ -17,7 +72,7 @@ if __name__ == '__main__':
         sys.exit()
 
     if arg == 'new':
-        pass
+        add_password()
     elif arg == 'list':
         pass
     elif arg == 'edit':
